e6b65b1ab249b20c7a478896a90c748f7554aad9
Jeremy-Fang <JeremyFang1123@gmail.com>
2020-05-23 23:40:43 -0400

    removed some files

--- 173/Q2.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def filterRestaurants(self, restaurants: List[List[int]], veganFriendly: int, maxPrice: int, maxDistance: int) -> List[int]:
-        v = sorted([i for i in sorted(restaurants, key=lambda i: i[0], reverse=True) \
-                    if (not veganFriendly or (veganFriendly and i[2] == veganFriendly)) \
-                    and i[3] <= maxPrice and i[4] <= maxDistance], \
-                   key=lambda i: i[1], reverse=True)
-        
-        return [i[0] for i in v]
--- 174/Q1.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        m = [(i, mat[i].count(1)) for i in range(len(mat))]
-        m = sorted(m, key=lambda i: i[1])
-        
-        return [m[i][0] for i in range(k)]
